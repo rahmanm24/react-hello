9ed64607b795208877f0afb1917f4dc6bbd5e0d1
rahmanm24 <rahmanm24@roehampton.ac.uk>
2023-02-13 02:53:29 +0000

    first component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import './App.css';
+import Employee from './components/Employee';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Employee />
         
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
---
+++ src/components/Employee.js
@@ -0,0 +1,5 @@
+function Employee(){
+    return <h2>Hey Baby...! this is an emloyee. Ula laaa.....!</h2>;
+}
+
+export default Employee;
